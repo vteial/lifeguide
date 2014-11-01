c46736971ba5fa1e7589d9848c6ab16f5346c795
vteial <vteial@gmail.com>
2014-11-01 14:02:38 +0530

    updating latest changes...

--- src/main/webapp/modules/test/testController.js
+++ src/main/webapp/modules/test/testController.js
@@ -2,6 +2,7 @@ function testController($rootScope, $scope, $log, $http) {
 	$rootScope.viewName = 'Test';
 
 	$scope.item = {
+		id : 0,
 		videoUrl : 'http://www.youtube.com/v/bBV9mzPSreU',
 		videoId : 'bBV9mzPSreU',
 		name : 'Introduction of Anatomic Therapy'
